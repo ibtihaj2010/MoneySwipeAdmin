15e44814e8fd57dd19b43b385c369b6761e9868b
ibtihaj <ibn@ciklum.com>
2019-05-18 17:45:12 +0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login', loadChildren: '../app/modules/login/login.module#LoginModule'
+  },
+  {
     path: 'dashboard', loadChildren: '../app/modules/dashboard/dashboard.module#DashboardModule'
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './modules/shared/shared.module';
@@ -10,6 +8,7 @@ import { SharedModule } from './modules/shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
+
 
   ],
   imports: [
